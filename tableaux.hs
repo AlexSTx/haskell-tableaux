e125e435ce3cfe3f6fe56a6b0159c3019059ee92
alexsander <alexsandersoarest@gmail.com>
2024-06-16 14:48:20 -0300

    many tweaks

--- tableaux.hs
+++ tableaux.hs
@@ -1,10 +1,11 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Redundant if" #-}
 {-# HLINT ignore "Redundant bracket" #-}
 import Data.Binary (Binary)
 import Data.Char (isAlpha, isSpace)
 import Data.List (find, isPrefixOf, stripPrefix)
-import Data.Maybe (maybe, isNothing, isJust)
+import Data.Maybe (isJust, isNothing, maybe)
 import Language.Haskell.TH.Ppr (hashParens)
 
 operators :: [String]
@@ -125,7 +126,6 @@ negateNode (Leaf operand hasParens) = UnaryOperation "~" (Leaf operand hasParens
 xor :: Bool -> Bool -> Bool
 xor p q = (p && not q) || (not p && q)
 
-
 data Type = Dysjunction | Conjunction | Terminal
   deriving (Show, Eq)
 
@@ -134,12 +134,12 @@ getType op isNeg
   | isNeg = case op of
       "->" -> Conjunction
       "&" -> Dysjunction
-      "/" -> Conjunction
+      "|" -> Conjunction
       _ -> Terminal
   | not isNeg = case op of
       "->" -> Dysjunction
       "&" -> Conjunction
-      "/" -> Dysjunction
+      "|" -> Dysjunction
       _ -> Terminal
 
 -- ESSES BOOLS AQUI NÃO SÃO SE TÁ NEGADO, SÃO O VALOR OFICIAL
@@ -148,12 +148,12 @@ getTypeData op isNeg
   | isNeg = case op of
       "->" -> (Conjunction, True, False)
       "&" -> (Dysjunction, False, False)
-      "/" -> (Conjunction, False, False)
+      "|" -> (Conjunction, False, False)
       _ -> (Terminal, False, False)
   | not isNeg = case op of
       "->" -> (Dysjunction, False, True)
       "&" -> (Conjunction, True, True)
-      "/" -> (Dysjunction, True, True)
+      "|" -> (Dysjunction, True, True)
       _ -> (Terminal, False, False)
 
 type IsNegated = Bool
@@ -163,137 +163,234 @@ data RefutationNode
   | RefLeaf String Type IsNegated (Maybe RefutationNode) (Maybe RefutationNode)
   deriving (Show, Eq)
 
-getInfoCarga :: Node -> (Operator, Node, Node)
-getInfoCarga (BinaryOperation operator first second _) = (operator, first, second)
-getInfoCarga (UnaryOperation {}) = error "Invalid Node Type - Unary"
-getInfoCarga (Leaf {}) = error "Invalid Node Type - Leaf"
+getInfoCarga :: Node -> (Operator, Maybe Node, Maybe Node)
+getInfoCarga (BinaryOperation operator first second _) = (operator, Just first, Just second)
+getInfoCarga (UnaryOperation operator _ _) = error "invalid operator"
+getInfoCarga (Leaf operator _) = (operator, Nothing, Nothing)
 
 isLeaf :: Node -> Bool
 isLeaf (Leaf {}) = True
 isLeaf _ = False
 
-createRefutationNode :: Node -> IsNegated -> Maybe (Node, Bool) -> Maybe RefutationNode
-createRefutationNode (UnaryOperation operator operand hasParens) isNegated unappliedRule = createRefutationNode operand (xor True isNegated) unappliedRule -- check
-createRefutationNode (Leaf operand hasParens) isNegated carga
+createRefutationNode :: Maybe Node -> IsNegated -> Maybe (Node, Bool) -> Maybe RefutationNode
+createRefutationNode Nothing _ _ = Nothing
+createRefutationNode (Just (UnaryOperation operator operand hasParens)) isNegated unappliedRule = createRefutationNode (Just operand) (xor True isNegated) unappliedRule
+createRefutationNode (Just (Leaf operand hasParens)) isNegated carga
   | isNothing carga = Just (RefLeaf operand Terminal isNegated Nothing Nothing)
   | isJust carga =
       do
-        ((operator, first, second), negadoCarga) <- case carga of
-          Just (node, negadoCarga) -> return (getInfoCarga node, negadoCarga)
+        ((operator, first, second), cargaNegada) <- case carga of
+          Just (node, cargaNegada) -> return (getInfoCarga node, cargaNegada) -- nao ta recebendo carga
           Nothing -> error "Expected a Node, but got Nothing"
-        Just (RefLeaf operand (getType operator negadoCarga) isNegated (createRefutationNode first False Nothing) (createRefutationNode second False Nothing))
-createRefutationNode (BinaryOperation operator operand1 operand2 hasParens) isNegated unappliedRule = do
+
+        Just
+          ( RefLeaf
+              operand
+              (getType operator (not cargaNegada))
+              isNegated
+              (createRefutationNode first False Nothing)
+              (createRefutationNode second False Nothing)
+          )
+createRefutationNode (Just (BinaryOperation operator operand1 operand2 hasParens)) isNegated unappliedRule = do
   let (tipo, b1, b2) = getTypeData operator isNegated
-  let transferirCarga = tipo == Dysjunction && not (isLeaf operand1)
+  let transferirCarga = tipo == Conjunction -- && not (isLeaf operand1)
   Just
     ( RefNode
         (BinaryOperation operator operand1 operand2 hasParens) -- node
         tipo -- type
-        (xor True isNegated) -- isNegated
-        (createRefutationNode operand1 (not b1 && False) Nothing) -- refutation node 1
-        (createRefutationNode operand2 (not b2 && False) (if transferirCarga then Just (operand1, not b1 && False) else Nothing)) -- refutation node 2
+        isNegated -- isNegated
+        (createRefutationNode (Just operand1) (not b1) Nothing) -- refutation node 1
+        (createRefutationNode (Just operand2) (not b2) (if transferirCarga then Just (operand1, not b1) else Nothing)) -- refutation node 2
     )
 
 createRefutationTree :: Node -> Maybe RefutationNode
-createRefutationTree arvore = createRefutationNode arvore False Nothing
+createRefutationTree arvore = createRefutationNode (Just arvore) False Nothing
 
 printRefutationTree :: Maybe RefutationNode -> String
 printRefutationTree (Just (RefNode node t isNeg esq dir)) = show (RefNode node t isNeg esq dir)
-
--- data RefutationNode
---   = RefNode Node Type IsNegated (Maybe RefutationNode) (Maybe RefutationNode)
---   | RefLeaf String Type IsNegated (Maybe RefutationNode) (Maybe RefutationNode)
---   deriving (Show, Eq)
-
--- type IsNegated = Bool
-
 
 -- Função auxiliar fromMaybe
 fromMaybe :: a -> Maybe a -> a
 fromMaybe defval Nothing = defval
 fromMaybe _ (Just val) = val
 
-refuta :: [(String, IsNegated)] -> RefutationNode -> Bool
-refuta variaveis (RefNode node tipo isNegated left right)
-  | tipo == Dysjunction = refuta
-                            variaveis
-                            (fromMaybe (RefNode node Terminal isNegated Nothing Nothing) left)
-                          &&
-                          refuta
-                            variaveis
-                            (fromMaybe (RefNode node Terminal isNegated Nothing Nothing) right)
-  | tipo == Conjunction = refuta
-                            variaveis
-                            (fromMaybe (RefNode node Terminal isNegated Nothing Nothing) left)
-                          ||
-                          refuta
-                            variaveis
-                            (fromMaybe (RefNode node Terminal isNegated Nothing Nothing) right)
+type FoundContradiction = Bool
+
+type VariableList = [(String, IsNegated)]
+
+invalidRefNode = RefNode (Leaf "a" False) Terminal False Nothing Nothing -- Refnode Terminal é invalido
+
+refuta :: VariableList -> RefutationNode -> FoundContradiction
+refuta variaveis (RefNode node tipo isNegated left right) -- apenas repassam variáveis pra baixo e aplicam regra
+  | tipo == Dysjunction =
+      ( refuta
+          variaveis
+          (fromMaybe invalidRefNode left)
+      )
+        && ( refuta
+               variaveis
+               (fromMaybe invalidRefNode right)
+           )
+  | tipo == Conjunction =
+      ( refuta
+          variaveis
+          (fromMaybe invalidRefNode left)
+      )
+        || ( refuta
+               variaveis
+               (fromMaybe invalidRefNode right)
+           )
   | otherwise = error "Invalid Tree"
 refuta [] (RefLeaf valor tipo isNegated left right)
-  | tipo == Terminal = False -- check
-  | tipo == Dysjunction = do
-      refuta
-        [(valor, isNegated)]
-        (fromMaybe (RefNode (Leaf valor False)  Terminal isNegated Nothing Nothing) left)
-      &&
-      refuta
-        [(valor, isNegated)]
-        (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) right)
-  | tipo == Conjunction = do
-      refuta
-        [(valor, isNegated)]
-        (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) left)
-      ||
-      refuta
-        [(valor, isNegated)]
-        (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) right)
-
+  | tipo == Terminal = False
+  | tipo == Dysjunction =
+      do
+        refuta
+          [(valor, isNegated)]
+          (fromMaybe invalidRefNode left)
+        && refuta
+          [(valor, isNegated)]
+          (fromMaybe invalidRefNode right)
+  | tipo == Conjunction =
+      do
+        refuta
+          [(valor, isNegated)]
+          (fromMaybe invalidRefNode left)
+        || refuta
+          [(valor, isNegated)]
+          (fromMaybe invalidRefNode right)
 refuta variaveis (RefLeaf valor tipo isNegated left right)
   | tipo == Terminal = do
-    let valorRecebido = lookup valor variaveis
-    if (isNothing valorRecebido || ((isJust valorRecebido) && xor (fromMaybe False valorRecebido) isNegated)) then True else False
+      let valorRecebido = lookup valor variaveis
+      if (isNothing valorRecebido || ((isJust valorRecebido) && not (xor (fromMaybe False valorRecebido) isNegated))) then False else True
 
-    --  variaveis ++ (valor, isNegated)
+  --  variaveis ++ (valor, isNegated)
   | tipo == Dysjunction = do
       let valorRecebido = lookup valor variaveis
-      if (isNothing valorRecebido || ((isJust valorRecebido) && xor (fromMaybe False valorRecebido) isNegated))
-        then True
-        else 
+      if (isNothing valorRecebido || ((isJust valorRecebido) && not (xor (fromMaybe False valorRecebido) isNegated)))
+        then False
+        else
           refuta
             (variaveis ++ [(valor, isNegated)])
-            (fromMaybe (RefNode (Leaf valor False)  Terminal isNegated Nothing Nothing) left)
-          &&
-          refuta
-            (variaveis ++ [(valor, isNegated)])
-            (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) right)
+            (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) left)
+            && refuta
+              (variaveis ++ [(valor, isNegated)])
+              (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) right)
   | tipo == Conjunction = do
       let valorRecebido = lookup valor variaveis
-      if (isNothing valorRecebido || ((isJust valorRecebido) && xor (fromMaybe False valorRecebido) isNegated))
-        then True
-        else 
+      if (isNothing valorRecebido || ((isJust valorRecebido) && not (xor (fromMaybe False valorRecebido) isNegated)))
+        then False
+        else
           refuta
             (variaveis ++ [(valor, isNegated)])
-            (fromMaybe (RefNode (Leaf valor False)  Terminal isNegated Nothing Nothing) left)
-          ||
-          refuta
-            (variaveis ++ [(valor, isNegated)])
-            (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) right)
-
+            (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) left)
+            || refuta
+              (variaveis ++ [(valor, isNegated)])
+              (fromMaybe (RefNode (Leaf valor False) Terminal isNegated Nothing Nothing) right)
 
 --------------------------------------------------------------------------------
 
+tree :: Maybe RefutationNode
+tree =
+  Just
+    ( RefNode
+        ( BinaryOperation
+            ("->")
+            (BinaryOperation ("|") (Leaf "p" False) (BinaryOperation ("&") (Leaf "q" False) (Leaf "r" False) (True)) (True))
+            (BinaryOperation ("&") (BinaryOperation ("|") (Leaf "p" False) (Leaf "q" False) (True)) (BinaryOperation ("|") (Leaf "p" False) (Leaf "r" False) (True)) (True))
+            (True)
+        )
+        (Conjunction)
+        (False)
+        ( Just
+            ( RefNode
+                ( BinaryOperation
+                    ("|")
+                    (Leaf "p" False)
+                    (BinaryOperation ("&") (Leaf "q" False) (Leaf "r" False) (True))
+                    (True)
+                )
+                (Dysjunction)
+                (True)
+                ( Just
+                    (RefLeaf ("p") (Terminal) (False) (Nothing) (Nothing))
+                )
+                ( Just
+                    ( RefNode
+                        ( BinaryOperation
+                            ("&")
+                            (Leaf "q" False)
+                            (Leaf "r" False)
+                            (True)
+                        )
+                        (Conjunction)
+                        (True)
+                        (Just (RefLeaf "q" Terminal False Nothing Nothing))
+                        (Just (RefLeaf "r" Terminal False Nothing Nothing))
+                    )
+                )
+            )
+        )
+        ( Just
+            ( RefNode
+                ( BinaryOperation
+                    ("&")
+                    (BinaryOperation "|" (Leaf "p" False) (Leaf "q" False) True)
+                    (BinaryOperation "|" (Leaf "p" False) (Leaf "r" False) True)
+                    (True)
+                )
+                (Dysjunction)
+                (False)
+                ( Just
+                    ( RefNode
+                        (BinaryOperation ("|") (Leaf "p" False) (Leaf "q" False) (True))
+                        (Conjunction)
+                        (False)
+                        (Just (RefLeaf "p" Terminal True Nothing Nothing))
+                        (Just (RefLeaf "q" Terminal True Nothing Nothing))
+                    )
+                )
+                ( Just
+                    ( RefNode
+                        (BinaryOperation ("|") (Leaf "p" False) (Leaf "r" False) (True))
+                        (Conjunction)
+                        (False)
+                        (Just (RefLeaf "p" Terminal True Nothing Nothing))
+                        (Just (RefLeaf "r" Terminal True Nothing Nothing))
+                    )
+                )
+            )
+        )
+    )
+
 equation :: String
-equation = "(p | (q & r)) -> ((p | q) & (p | r))" :: String
+-- equation = "(p | (q & r)) -> ((p | q) & (p | r))" :: String
+
+equation = "a|~a" -- expected: False
+
+-- equation = "(a->b)&a"
+
 -- equation = "(p -> q)" :: String
 
 main :: IO ()
 main = do
-  let arvore = createSyntaxTree equation
-  -- print (printSyntaxTree arvore)
-  let refutationTree = createRefutationTree arvore
+  let arvore = negateNode (createSyntaxTree equation)
+  -- print (printSyntaxTree (negateNode arvore))
+  print (printSyntaxTree arvore)
+
+  let refutationTree = createRefutationTree (arvore)
   print (show refutationTree)
-  -- case refutationTree of
-  --   Nothing -> print ("Entrada Invalida") 
-  --   Just refutationTree -> print (show (refuta [] refutationTree)) 
-  
+
+  case refutationTree of
+    Nothing -> print ("Entrada Invalida")
+    Just refutationTree -> print (show (refuta [] refutationTree))
+
+smol =
+  Just
+    ( RefNode
+        (BinaryOperation "|" (Leaf "a" False) (UnaryOperation "~" (Leaf "a" False) True) True)
+        Conjunction
+        True
+        (Just (RefLeaf "a" Terminal True Nothing Nothing))
+        (Just (RefLeaf "a" Terminal False Nothing Nothing))
+    )
